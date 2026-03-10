b6417355aa35d7879a54f9ac027701c1cc4907e1
MarcosViniciusDomingues <mvcdomingues@gmail.com>
2026-03-10 03:31:21 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const yearEls = document.querySelectorAll(".year");
-  const year = new Date().getFullYear();
-  yearEls.forEach(el => el.textContent = year);
-});
